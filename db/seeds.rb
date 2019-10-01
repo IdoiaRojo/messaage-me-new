d8575b2d65e21a3c12bc4365c384ae3fd8085cd8
idoia <idoia@ifeelonline.com>
2019-10-01 10:45:49 +0200

    third commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Idoia02", password: "password")
+User.create(username: "Idoia03", password: "password")
+User.create(username: "Idoia04", password: "password")
+User.create(username: "Idoia05", password: "password")
+
+Message.create(body: ":)", user: User.find(2))
+Message.create(body: "qué tal la semanita?", user: User.find(4))
+Message.create(body: "pues aquí tranquilita", user: User.find(3))
+Message.create(body: "nos vemos este finde¿¿¿", user: User.find(1))
+Message.create(body: "vienen mis suegris!así que siii", user: User.find(2))
